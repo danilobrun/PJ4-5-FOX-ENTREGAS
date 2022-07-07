a4950ba5eeb97e7bdf82dfb43f4b3bd7649712a3
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-07-06 21:55:19 -0300

    instala styled-components e seu arquivo de tipagem @types

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,17 @@
 import { Button, Container } from "react-bootstrap";
+import styled from 'styled-components'
 
 function App() {
   return (
     <Container>
-      <div>Fox Entregas</div>
+      <Title>Fox Entregas</Title>
       <Button>teste</Button>
     </Container>
   );
 }
+
+const Title = styled.p`
+  font-size: 3rem;
+`
 
 export default App;
