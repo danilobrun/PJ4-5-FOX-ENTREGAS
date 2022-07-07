4e2240b5ce98afa4d4fc0760cb1cf9de82a3064a
Danilou Souza Brun <danilobrun@Danilous-MacBook-Pro.local>
2022-07-07 11:08:05 -0300

    instala react-toastify

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +13,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer />
     </BrowserRouter>
   </React.StrictMode>
 );
