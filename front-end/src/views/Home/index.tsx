85ea5ab997599be220cf9e94df354abf181b28a0
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-07-13 21:27:32 -0300

    cria a HomeView() e cria nosso componente de botão

--- front-end/src/views/Home/index.tsx
+++ front-end/src/views/Home/index.tsx
@@ -1,19 +1,42 @@
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Container } from "react-bootstrap";
-import { toast } from "react-toastify";
+import { Container } from 'react-bootstrap'
 import styled from 'styled-components'
+import bgMobile from "../../assets/img/logo-motoboy-mobile.png";
+import bgDesktop from "../../assets/img/logo-motoboy-desktop.png";
+import { CustomButton } from '../../components/CustomButton';
 
 export function HomeView () {
     return (
-        <Container>
-          <Title>Fox Entregas</Title>
-          <Button onClick={() => toast.success('Teste!')}>teste</Button>
-          <FontAwesomeIcon icon={faFacebookSquare} />
+      <Banner className='vh-100'>
+          <Container className="h-100 d-flex flex-column justify-content-center align-items-center align-items-lg-start">
+            <Title className="text-white text-center text-lg-start mt-auto mt-lg-0">Cifra Engenharia Logística, moto entrega.</Title>
+            <CustomButton variant='success' size='lg' className='mt-auto mt-lg-3 mb-2'>Criar conta</CustomButton>
+            <CustomButton variant='success' size='lg' className='mb-4'>Fazer Login</CustomButton>
         </Container>
+      </Banner>
     )
 }
 
-const Title = styled.p`
-  font-size: 3rem;
+const Banner = styled.div`
+  background: url(${bgMobile}) no-repeat center center;
+  background-size: contain;
+  /* background-size: cover; */
+  @media (min-width: 576px) {
+    background-image: url(${bgDesktop});
+  }
+  @media (min-width: 768px) {
+    background-image: url(${bgMobile});
+  }
+  @media (min-width: 992px) {
+    background-image: url(${bgDesktop});
+  }
 `
+
+const Title = styled.h1`
+  font-size: 2.25rem;
+  font-weight: bold;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  @media (min-width: 992px) {
+    font-size: 3rem;
+    max-width: 500px;
+  }
+  `
