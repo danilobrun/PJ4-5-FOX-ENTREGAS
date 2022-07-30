0620cdac5db73f512fc8f9f1adedc2db36619872
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-07-29 22:20:30 -0300

    implementa layout com header mais scroll

--- front-end/src/components/Layout/Header.tsx
+++ front-end/src/components/Layout/Header.tsx
@@ -1,22 +1,41 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../../assets/img/logo-cifra.png"
+import LogoWhite from "../../assets/img/logo-cifra-white.png"
 import { CustomButton } from "../CustomButton";
 
 export function Header () {
+    const [isTransparent, setIsTransparent] = useState(true)
+    useEffect(() => {
+        const scrollChange = () => {
+            const isLowScroll = window.scrollY < 60
+            if (isLowScroll !==  isTransparent) {
+                setIsTransparent(isLowScroll)
+            }
+        }
+        window.addEventListener('scroll', scrollChange)
+        return () => {
+            window.removeEventListener('scroll', scrollChange)
+        }
+    }, [isTransparent])
     return (
-        <Navbar fixed='top' expand="lg">
+        <Navbar fixed='top' expand="lg" bg={isTransparent ? undefined : 'secondary'}>
             <Container>
                 <Navbar.Brand to='/' as={Link}>
-                    <img src={Logo} width={194} height={51} alt="Logo Cifra"/>
+                    <img src={isTransparent ? Logo : LogoWhite} width={194} height={51} alt="Logo Cifra"/>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <NavbarToggleStyled aria-controls="basic-navbar-nav">
+                    <FontAwesomeIcon icon={faBars} className={isTransparent ? "text-gray" : "text-white"} size="lg" />
+                </NavbarToggleStyled>
                 <NavbarCollapseStyled id="basic-navbar-nav" className="justify-content-center text-center">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Inicio</Nav.Link>
-                        <CustomButton>Criar Conta</CustomButton>
-                        <CustomButton>Fazer Login</CustomButton>
+                    <Nav className="ms-auto">
+                        <NavLinkStyled forwardedAs={Link} to='/'>Inicio</NavLinkStyled>
+                        <CustomButton className="mt-2 mt-lg-0 ms-lg-4">Criar Conta</CustomButton>
+                        <CustomButton className="mt-2 mt-lg-0 ms-lg-4">Fazer Login</CustomButton>
                     </Nav>
                 </NavbarCollapseStyled>
             </Container>
@@ -24,10 +43,20 @@ export function Header () {
     )
 }
 
+const NavbarToggleStyled = styled(Navbar.Toggle)`
+    border: none;
+`
+
 const NavbarCollapseStyled = styled(Navbar.Collapse)`
     @media (max-width: 991px) {
         background-color: #FFF;
         margin: 0 -12px;
         padding: 1rem 2rem;
     }
+`
+
+const NavLinkStyled = styled(Nav.Link)`
+ @media (min-width: 992px) {
+    color: #FFF !important;
+ }
 `
