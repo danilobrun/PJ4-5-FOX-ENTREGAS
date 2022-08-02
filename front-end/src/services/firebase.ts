635379af2db9101153680cc4ec2fb43135ce0dbb
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-08-02 10:57:59 -0300

    instala firebase

---
+++ front-end/src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDC1tfFdYmvWRwFKMhcylESCEQs1LvAXao",
+  authDomain: "cifra-logistica-aed50.firebaseapp.com",
+  projectId: "cifra-logistica-aed50",
+  storageBucket: "cifra-logistica-aed50.appspot.com",
+  messagingSenderId: "761469896465",
+  appId: "1:761469896465:web:f0c98c5b18e91fb7b5aeb0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
