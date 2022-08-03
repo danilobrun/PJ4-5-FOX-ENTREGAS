33dded5fb2678e66d27f8185bdd61f3fd3d9906d
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-08-03 19:10:00 -0300

    atualiza formulário controlado +iMask tipa campos dos formulários

--- front-end/src/views/Register/index.tsx
+++ front-end/src/views/Register/index.tsx
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { CustomButton } from "../../components/CustomButton";
@@ -5,48 +6,75 @@ import { FormField } from "../../components/FormField";
 import { Layout } from "../../components/Layout";
 import { PageTitle } from "../../components/PageTitle";
 
+type FormValues = {
+    name: string
+    email: string
+    phone: string
+    password: string
+    agree: boolean
+}
+
 export function RegisterView () {
+    const formik = useFormik<FormValues>({
+        initialValues: {
+            name: '',
+            email: '',
+            phone: '',
+            password: '',
+            agree: false
+        },
+        onSubmit: (values) => 
+        console.log('oi', values)
+    })
+    const getFieldProps = (fieldName: keyof FormValues) => {
+        return {
+            ...formik.getFieldProps(fieldName),
+            controlId: `input-${fieldName}`
+        }
+    }
     return (
         <Layout>
             <Container>
                 <Row className="justify-content-center">
                     <Col lg={4}>
                         <PageTitle>Nova Conta</PageTitle>
-                        <Form>
+                        <Form onSubmit={formik.handleSubmit}>
                             <FormField
                                 label="Nome"
                                 placeholder="Digite aqui seu nome"
-                                controlId="input-name"
+                                {...getFieldProps('name')}
                             />
                             <FormField
                                 label="E-mail"
                                 placeholder="Ele será seu ususário"
-                                controlId="input-email"
                                 type="email"
+                                {...getFieldProps('email')}
                             />
                             <FormField
                                 label="Telefone"
                                 placeholder="(00) 00000-0000"
-                                controlId="input-phone"
+                                {...getFieldProps('phone')}
                                 mask={[
                                     { mask: '(00) 0000-0000' },
                                     { mask: '(00) 00000-0000' },
                                 ]}
+                                onAccept={value => formik.setFieldValue ('phone', value)}
                             />
                             <FormField
                                 label="Senha"
                                 placeholder="Informe sua senha de acesso"
-                                controlId="input-password"
+                                {...getFieldProps('password')}
                                 type="password"
                             />
                             <Form.Group className="mb-3" controlId="input-agree">
                                 <Form.Check
+                                    {...getFieldProps('agree')}
                                     type="checkbox"
                                     label={<>Eu li e aceito os <a href="/termos-de-uso.pdf" target="_blank">Termos de Uso.</a></>}
                                 />
                             </Form.Group>
                             <div className="d-grid mb-4">
-                                <CustomButton>
+                                <CustomButton type="submit">
                                     Criar conta
                                 </CustomButton>
                             </div>
