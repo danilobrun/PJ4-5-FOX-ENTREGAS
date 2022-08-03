8e383a5a42a0bb4bb75c7dc7e1b39cb1abf62103
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-08-03 12:57:53 -0300

    ajusta tela de cadastro aplicando layout bootstrap de row e col

--- front-end/src/views/Register/index.tsx
+++ front-end/src/views/Register/index.tsx
@@ -1,4 +1,6 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { CustomButton } from "../../components/CustomButton";
 import { FormField } from "../../components/FormField";
 import { Layout } from "../../components/Layout";
 import { PageTitle } from "../../components/PageTitle";
@@ -7,16 +9,51 @@ export function RegisterView () {
     return (
         <Layout>
             <Container>
-                <PageTitle>Nova Conta</PageTitle>
-                <FormField 
-                controlId="userName"
-                label="name"
-                placeholder="Digite o seu nome"
-                error="Preencha o seu nome."
-                isInvalid
-                mask={[
-                    {mask: '000.000.000-00'}
-                ]}/>
+                <Row className="justify-content-center">
+                    <Col lg={4}>
+                        <PageTitle>Nova Conta</PageTitle>
+                        <Form>
+                            <FormField
+                                label="Nome"
+                                placeholder="Digite aqui seu nome"
+                                controlId="input-name"
+                            />
+                            <FormField
+                                label="E-mail"
+                                placeholder="Ele será seu ususário"
+                                controlId="input-email"
+                                type="email"
+                            />
+                            <FormField
+                                label="Telefone"
+                                placeholder="(00) 00000-0000"
+                                controlId="input-phone"
+                                mask={[
+                                    { mask: '(00) 0000-0000' },
+                                    { mask: '(00) 00000-0000' },
+                                ]}
+                            />
+                            <FormField
+                                label="Senha"
+                                placeholder="Informe sua senha de acesso"
+                                controlId="input-password"
+                                type="password"
+                            />
+                            <Form.Group className="mb-3" controlId="input-agree">
+                                <Form.Check
+                                    type="checkbox"
+                                    label={<>Eu li e aceito os <a href="/termos-de-uso.pdf" target="_blank">Termos de Uso.</a></>}
+                                />
+                            </Form.Group>
+                            <div className="d-grid mb-4">
+                                <CustomButton>
+                                    Criar conta
+                                </CustomButton>
+                            </div>
+                            <p className="text-center">Já possui contar?<br/> <Link to='/login'>Entrar</Link></p>
+                        </Form>
+                    </Col>
+                </Row>
             </Container>
         </Layout>
     )
